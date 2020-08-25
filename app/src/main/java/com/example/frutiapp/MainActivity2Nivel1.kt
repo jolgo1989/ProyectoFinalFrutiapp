7644ec98d685b0b86e58d8bcf57ace7d0325a4d6
jorgeprueba <jgamarra@misena.edu.co>
2020-08-25 18:03:54 -0500

    Se agrego imagen aleatorias en el imageview

--- app/src/main/java/com/example/frutiapp/MainActivity2Nivel1.kt
+++ app/src/main/java/com/example/frutiapp/MainActivity2Nivel1.kt
@@ -1,12 +1,14 @@
 package com.example.frutiapp
 
+import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main_activity2_nivel1.*
 
-class MainActivity2Nivel1 : AppCompatActivity() {
+
+open class MainActivity2Nivel1 : AppCompatActivity() {
     var score = 0
     var numAleatorio_uno: Int = 0
     var numAleatorio_dos: Int = 0
@@ -43,11 +45,49 @@ class MainActivity2Nivel1 : AppCompatActivity() {
         mp.isLooping
 
         //Metodo para indicar sonido great y bad
-        mp_great = MediaPlayer.create(this,R.raw.wonderful)
-        mp_bad = MediaPlayer.create(this,R.raw.bad)
+        mp_great = MediaPlayer.create(this, R.raw.wonderful)
+        mp_bad = MediaPlayer.create(this, R.raw.bad)
+
+        numAleAtorio()
 
 // i have added toast
         Toast.makeText(this, "Nivel 1 - Sumas basicas", Toast.LENGTH_SHORT).show()
 
+    }
+
+    // Metodo para pasar al siguiente nivel
+     fun numAleAtorio() {
+
+        if (score <= 9) {
+            numAleatorio_uno = (Math.random() * 10).toInt()
+            numAleatorio_dos = (Math.random() * 10).toInt()
+
+            resultado = numAleatorio_uno + numAleatorio_dos
+//Metodo para cambiar las imagenes del imageview
+            if (resultado <= 10) {
+                for (i in numero.indices) {
+                    val id = resources.getIdentifier(numero[i],"drawable", packageName)
+                    if (numAleatorio_uno == i) {
+                        imageViewnumber1.setImageResource(id)
+                    }
+                    if (numAleatorio_dos == i) {
+                        imageViewnumber2.setImageResource(id)
+                    }
+                }
+            } else {
+                numAleAtorio()
+            }
+        } else {
+            val intent = Intent(this, MainActivity2Nivel2::class.java)
+            string_score = score.toString()
+            string_vidas = vidas.toString()
+            intent.putExtra("jugador", textViewNombre.text.toString())
+            intent.putExtra("score", string_score)
+            intent.putExtra("vidas", string_vidas)
+            startActivity(intent)
+            finish()
+            mp.stop()
+            mp.release()
+        }
     }
 }
